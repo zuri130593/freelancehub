bec3349e856fbae2cda2c913cd49118c29407874
Irving Alejandro Zuñiga Rivera <zuri130593@gmail.com>
2026-05-07 10:59:31 -0600

    chore: setup freelancehub project

---
+++ src/main/java/com/freelancehub/FreelancehubApplication.java
@@ -0,0 +1,13 @@
+package com.freelancehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelancehub/FreelancehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
